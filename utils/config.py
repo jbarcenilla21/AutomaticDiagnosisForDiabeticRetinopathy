0574be0f792b7cc1d3939293fb74cf72eccdb5a3
Jorge Barcenilla <jbarcenilla2014@gmail.com>
2026-03-25 15:24:11 +0100

    Initial project structure for Automatic Diagnosis of Diabetic Retinopathy

---
+++ utils/config.py
@@ -0,0 +1,13 @@
+# CONFIG FILE
+
+
+# ------- training hyperparameters -------
+learning_rate   = 1e-3
+batch_size      = 32
+num_epochs      = 50
+
+# ------- CONSTANTS -------
+img_height      = 224       # input image height
+img_width       = 224       # input image width
+num_channels    = 3         # RGB images
+num_classes     = 5         # DR severity grades: 0 (No DR), 1, 2, 3, 4
---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+# UTILS FILE
+
+import torch
+from torchvision import transforms
+
+
+def get_transforms(train: bool = True):
+    """Returns image transforms for training or evaluation."""
+    if train:
+        return transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomVerticalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
+        ])
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225]),
+    ])
